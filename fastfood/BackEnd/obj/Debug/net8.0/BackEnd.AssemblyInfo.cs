0ea0786a5afb6f31a66e33e55180c048370e06fb
cdmasd <tienpdps28044@fpt.edu.vn>
2024-07-08 09:27:17 +0700

    FIX (UI) : TokenProvider SD => StaticDetails

--- fastfood/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ fastfood/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7384bc95180bebecb329f57f41a32a94417d1c87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bad4d9137a858e0e1661a83900ba053ba8959f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
