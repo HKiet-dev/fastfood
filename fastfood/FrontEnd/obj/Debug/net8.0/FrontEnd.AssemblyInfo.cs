1713a8fbcff80435bfc3758834f0c2e98b52e955
HuynhKietFPTedu <huynhkiet.dev@gmail.com>
2024-07-09 14:33:05 +0700

    a

--- fastfood/FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
+++ fastfood/FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2d9d04e42da3d5f65b6d7f60cb9366e26e6847")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d982b5fbef5368e6af7cd8283f7d6301cc14701")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
