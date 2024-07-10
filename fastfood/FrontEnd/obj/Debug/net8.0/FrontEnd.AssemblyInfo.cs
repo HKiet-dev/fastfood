d4fc9993d42672dc0f181e16c9d4c6cd7b1ca99d
cdmasd <tienpdps28044@fpt.edu.vn>
2024-07-10 08:48:17 +0700

    Remove Forecast

--- fastfood/FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
+++ fastfood/FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a466ae16cd1500e2ebe9a933f4fedefb7b91ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0293714f80dd8d7a5c04ed842893ea1363f3ca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
