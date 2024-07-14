f4e2e9088060cb79a3d3c7ab17c702653c204cd3
hoang truong <hoangtmps35865@fpt.edu.vn>
2024-07-14 14:07:24 +0700

    list product cart details

--- fastfood/FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
+++ fastfood/FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99402f4b3a1fc54d3109778784479a2b7b09b8d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d44865c8d0a7a3ae85342f3205793c57377110b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
