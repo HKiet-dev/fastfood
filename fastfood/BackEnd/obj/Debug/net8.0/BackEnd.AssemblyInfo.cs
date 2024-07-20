8719f599d1a4b5261faad01f9e90b377286ebdc3
cdmasd <tienpdps28044@fpt.edu.vn>
2024-07-20 14:58:32 +0700

    DESC (SERVER) : API Docs v1

--- fastfood/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ fastfood/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb3173319f9e5a3f6c46506d6ec2902c4ffea4aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5708d48c55dbab8abf0218bd6bba1e25198c628f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
