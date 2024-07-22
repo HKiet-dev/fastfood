469132706661ad07bc5ddea9de22ddf0620f37f4
HuynhKietFPTedu <huynhkiet.dev@gmail.com>
2024-07-22 19:12:41 +0700

    Update CartDetail

--- fastfood/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ fastfood/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49e36ec7cb6e807959d5ca38e85bcae83dcdb44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d60b101012fcecf13fc68e0477a0c034d2cd7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
