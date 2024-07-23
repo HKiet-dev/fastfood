e77617308f80f46615e7c0467ab89a64f941fa75
cdmasd <tienpdps28044@fpt.edu.vn>
2024-07-23 09:38:39 +0700

    UPDATE (SERVER) : Order & OrderDetail

--- fastfood/BackEnd/Models/Dtos/OrderDetailDto.cs
+++ fastfood/BackEnd/Models/Dtos/OrderDetailDto.cs
@@ -7,13 +7,11 @@ namespace BackEnd.Models.Dtos
     {
         public int OrderId { get; set; }
         public int ProductId { get; set; }
-        [Column(TypeName = "DECIMAL(18,2)")]
-        [Range(0, double.MaxValue, ErrorMessage ="Giá sản phẩm phải lớn hơn bằng 0")]
+        [Required, Range(1000, 1000000), Column(TypeName = "decimal(18,2)")]
         public decimal UnitPrice { get; set; }
+        [Required]
         public int Quantity { get; set; }
-        [Range(1,100,ErrorMessage = "Giảm giá chỉ có thể trong khoảng từ 1 đến 100")]
-        public int Discount { get; set; }
-        [Column(TypeName = "DECIMAL(18,2)")]
+        [Required, Range(1000, 1000000), Column(TypeName = "decimal(18,2)")]
         public decimal Total { get; set; }
     }
 }
