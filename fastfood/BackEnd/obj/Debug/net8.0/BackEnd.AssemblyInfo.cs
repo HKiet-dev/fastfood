941d175c404ac78c9e0965f036f46e858b8b1fdb
HuynhKietFPTedu <huynhkiet.dev@gmail.com>
2024-07-23 16:21:58 +0700

    update ignore

--- fastfood/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ fastfood/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3af3258bf1b800948b8ce6333d281cc0bb34358b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e429b7e09181fff1ef0d2017146123068a3c1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
